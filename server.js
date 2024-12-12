6450f1648e7170521f587c2d0fd578289de8047a
dipto-kainin <kaininhop@gmail.com>
2024-12-13 01:33:51 +0530

    new file:   config/db.js 	new file:   middlewere/auth.js 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`server started on port ${port}`));
